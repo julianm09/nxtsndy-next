6149dd4fdafe1598fec04d2318647f556d606bd0
Julian Mayes <juliantmayes@gmail.com>
2021-07-29 19:51:12 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -201,14 +201,14 @@ export default function Home({ dark, scrollPosition }) {
 
         <ContainerUI>
           <ImageUI style={{top: -scrollPosition / 10 }}>
-            <img style={{width: '100%'}} src="/profile1.jpg"/>
+            <Image width="100%" height="100%" src="/profile1.jpg"/>
             <SubHeaderUI >
               Sota Mori 
             </SubHeaderUI>
           </ImageUI>
 
           <ImageUI style={{top: -scrollPosition / 15 , left: scrollPosition / 10}}>
-            <img style={{width: '100%'}} src="/profile2.jpg"/>
+            <Image width="100%" height="100%" src="/profile2.jpg"/>
             <SubHeaderUI >
               Tom Powers
             </SubHeaderUI>
