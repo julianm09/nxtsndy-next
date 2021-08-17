271678f7e1228157f174113185eefb4b191ed4fd
Julian Mayes <juliantmayes@gmail.com>
2021-08-17 11:15:25 -0700

    songs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.wav$/,
+      use: {
+        loader: 'url-loader',
+      },
+    });
+    return config;
+  },
 }
